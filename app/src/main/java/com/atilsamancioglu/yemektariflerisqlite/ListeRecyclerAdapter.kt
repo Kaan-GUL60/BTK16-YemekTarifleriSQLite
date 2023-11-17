4d3eac353bbdea6fe2d11c1040a55b8f784c061c
GKaan783 <97687364+Kaan-GUL60@users.noreply.github.com>
2023-11-17 19:55:38 +0300

    Update ListeRecyclerAdapter.kt
    adding yorum

--- app/src/main/java/com/atilsamancioglu/yemektariflerisqlite/ListeRecyclerAdapter.kt
+++ app/src/main/java/com/atilsamancioglu/yemektariflerisqlite/ListeRecyclerAdapter.kt
@@ -8,14 +8,14 @@ import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.recycler_row.view.*
 
 class ListeRecyclerAdapter(val yemekListesi: ArrayList<String>, val idListesi : ArrayList<Int>) : RecyclerView.Adapter<ListeRecyclerAdapter.YemekHolder>() {
-
+//adapter sınıfı oluturuldu, 
     class YemekHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): YemekHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.recycler_row,parent,false)
+        val view = inflater.inflate(R.layout.recycler_row,parent,false) //recycler_row xmlden çağırıldı ve ilgili görünrtü sınıfı olarak döndürüldü unutma ekledğin grüntü sınıfı custmo olan xml de
         return YemekHolder(view)
     }
 
@@ -23,9 +23,9 @@ class ListeRecyclerAdapter(val yemekListesi: ArrayList<String>, val idListesi : 
         return yemekListesi.size
     }
 
-    override fun onBindViewHolder(holder: YemekHolder, position: Int) {
-        holder.itemView.recycler_row_text.text = yemekListesi[position]
-        holder.itemView.setOnClickListener {
+    override fun onBindViewHolder(holder: YemekHolder, position: Int) { //customdaki doldurulacak yerlere erişim sağlayaıp verileri eklyioruz
+        holder.itemView.recycler_row_text.text = yemekListesi[position]  //row_text ile custom viewer a ulaşıp oralara gelecek olan veriler işleniyor gönderiliyor  listwiew de gösterilmesi için
+        holder.itemView.setOnClickListener {     // arrayliste e ilgili position a yerleştiriliyorlar listwiev de
             val action = ListeFragmentDirections.actionListeFragmentToTarifFragment("recyclerdangeldim",idListesi[position])
             Navigation.findNavController(it).navigate(action)
         }
